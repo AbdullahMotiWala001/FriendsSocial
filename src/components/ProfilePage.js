eef8c605cc047332e548e70c2398ebd2bfea66fc
Abdullah-MotiWala <motiwalaabdullah100@gmail.com>
2021-12-31 03:23:07 +0500

    profile page ui balanced

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,70 +1,94 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import EditSharpIcon from "@mui/icons-material/EditSharp";
+import TextField from "@mui/material/TextField";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 
 export default function TemporaryDrawer() {
-    const [state, setState] = React.useState(false)
-
-    const toggleDrawer = (open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-        setState(open);
+  const [state, setState] = React.useState(false);
+  const toggleDrawer = open => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
     }
+    setState(open);
+  };
 
-    const list = () => (
-        <Box
-            sx={{ width: '100vw' }}
-            role="presentation"
+  //making ui for profile page
+  const [expanded, setExpanded] = React.useState(false);
 
-
+  const handleChange = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+  const accordianUi = () => {
+    return (
+      <Box
+        sx={{ width: "100vw" }}
+        role="presentation"
         //closing drawer
         // onClick={toggleDrawer(false)}
         onKeyDown={toggleDrawer(false)}
+      >
+        <Accordion
+          expanded={expanded === "panel1"}
+          onChange={handleChange("panel1")}
         >
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>
-                            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
+          <AccordionSummary
+            expandIcon={<EditSharpIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+          >
+            <Typography sx={{ color: "text.secondary" }}>
+              {/* {name here} */}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              <TextField
+                id="input-with-icon-textfield"
+                // label="TextField"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="standard"
+              />
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      </Box>
     );
+  };
+  //
 
-    return (
-        <div>
-            <React.Fragment key={1}>
-                <Button onClick={toggleDrawer(true)}>Profile</Button>
-                <Drawer
-                    open={state}
-                    onClose={toggleDrawer(false)}
-                >
-                    {list()}
-                </Drawer>
-            </React.Fragment>
-        </div>
-    );
+  return (
+    <div>
+      <React.Fragment key={1}>
+        <Button onClick={toggleDrawer(true)}>Profile</Button>
+        <Drawer open={state} onClose={toggleDrawer(false)}>
+          {accordianUi()}
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
 }
